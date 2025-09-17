8d0a55e410c79292bd9c131ce83b31919bdc7afc
Biixie <65206220+ibiixie@users.noreply.github.com>
2025-09-17 10:44:44 +0200

    feat: update backup policy, broadcast in-game backup notification

---
+++ pre-backup.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+rcon-cli "say <font:uniform><red><b><i>Server restart in 15 minutes. Estimated downtime is ~5 minutes.</i></b></red></font>"
+
+sleep 5m
+
+rcon-cli "say <font:uniform><red><b><i>Server restart in 10 minutes. Estimated downtime is ~5 minutes.</i></b></red></font>"
+
+sleep 5m
+
+rcon-cli "say <font:uniform><red><b><i>Server restart in 5 minutes. Estimated downtime is ~5 minutes.</i></b></red></font>"
+
+sleep 1m
+
+rcon-cli "say <font:uniform><red><b><i>Server restart in 4 minutes. Estimated downtime is ~5 minutes.</i></b></red></font>"
+
+sleep 1m
+
+rcon-cli "say <font:uniform><red><b><i>Server restart in 3 minutes. Estimated downtime is ~5 minutes.</i></b></red></font>"
+
+sleep 1m
+
+rcon-cli "say <font:uniform><red><b><i>Server restart in 2 minutes. Estimated downtime is ~5 minutes.</i></b></red></font>"
+
+sleep 1m
+
+rcon-cli "say <font:uniform><red><b><i>Server restart in 1 minute. Estimated downtime is ~5 minutes.</i></b></red></font>"
+
+sleep 30s
+
+rcon-cli "say <font:uniform><red><b><i>Server restart in 30 seconds. Estimated downtime is ~5 minutes.</i></b></red></font>"
+
+sleep 20s
+
+rcon-cli "say <font:uniform><red><b><i>Server restart in 10 seconds. Estimated downtime is ~5 minutes.</i></b></red></font>"
+
+sleep 5
+
+rcon-cli "say <font:uniform><red><b><i>Server restart in 5 seconds. Estimated downtime is ~5 minutes.</i></b></red></font>"
+
+sleep 1
+
+rcon-cli "say <font:uniform><red><b><i>4</i></b></red></font>"
+
+sleep 1
+
+rcon-cli "say <font:uniform><red><b><i>3</i></b></red></font>"
+
+sleep 1
+
+rcon-cli "say <font:uniform><red><b><i>2</i></b></red></font>"
+
+sleep 1
+
+rcon-cli "say <font:uniform><red><b><i>1</i></b></red></font>"
+
+sleep 1
